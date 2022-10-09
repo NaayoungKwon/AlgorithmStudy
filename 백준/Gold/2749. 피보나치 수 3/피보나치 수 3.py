a1f67adc90fb67068a3fbfb8bc8e2a81cac87521
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2022-10-09 18:16:38 +0900

    [Gold II] Title: 피보나치 수 3, Time: 496 ms, Memory: 195128 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def fibo(n):
+    data = [0]*15000001;
+    data[0] = 0;
+    data[1] = 1;
+    i = 0;
+    
+    k = n % 1500000;
+    while i < k:
+        data[i+2] = (data[i]+ data[i+1]) % 1000000;
+        i += 1;
+    
+    return data[k];
+  
+n = int(sys.stdin.readline());
+print(fibo(n));
