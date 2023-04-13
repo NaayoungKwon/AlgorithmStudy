b081136d00c0ea931d69e5398f0ca509a524b8ee
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2023-04-14 00:18:12 +0900

    [Silver II] Title: 나무 자르기, Time: 608 ms, Memory: 262272 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,26 @@
+import sys
+
+def cut(k,trees):
+    cnt = 0
+    for tree in trees:
+        cnt += max(0,tree-k)
+    return cnt;
+
+def solution(n,m,trees,s,e):
+
+    if s >= e:
+        return e;
+
+    k = (s + e) // 2;
+    cnt = cut(k,trees)
+
+    if m <= cnt:
+        if s == k or k == e:
+            return k
+        return max(k, solution(n,m,trees,k,e))
+    else:
+        return max(0, solution(n,m,trees,s,k))
+
+n,m = map(int,sys.stdin.readline().split());
+myList = list(map(int,sys.stdin.readline().split())) ;
+print(solution(n, m,myList,0,sum(myList)));
