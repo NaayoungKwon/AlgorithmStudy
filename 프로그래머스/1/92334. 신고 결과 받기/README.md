08870e2f9ae9a85f3abb5f509b62194253acab8f
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2024-09-21 09:11:50 +0900

    [level 1] Title: 신고 결과 받기, Time: 167.78 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 1.61 ms
+메모리: 177 MB, 시간: 167.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 13:14:39
+2024년 09월 21일 09:11:48
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,41 +1,41 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
+        // Map<신고자, Set<당한사람>>
+        // Map<당한사람, 횟수>
+        // report를 순회하면서 신고자에 set이 있으면 확인해서 없을 때 추가
+        // 순회 완료 후 map에서 k 이상인 것을 Set으로 다시 모아둠
+        // 첫 번째 Map의 값과 합집합 사이즈를 저장
         Map<String, Set<String>> reportMap = new HashMap<>();
-        Map<String, Integer> nameIdx = new HashMap<>();
-
-        for(String repo : report){
-          String[] splitted = repo.split(" ");
-          String reporter = splitted[0];
-          String reported = splitted[1];
-
-          if(reportMap.containsKey(reported)) {
-            reportMap.get(reported).add(reporter);
-          } else{
-            HashSet<String> newSet = new HashSet<>();
-            newSet.add(reporter);
-            reportMap.put(reported,newSet );
-          }
+        Map<String, Integer> alarmCnt = new HashMap<>();
+        
+        for(int i = 0 ; i < id_list.length ; i++){
+            reportMap.put(id_list[i], new HashSet<>());
         }
-
-        for(int i = 0; i < id_list.length; i++){
-          nameIdx.put(id_list[i], i);
-          answer[i] = 0;
+        for(int i = 0 ; i < report.length ; i++){
+            String[] sl = report[i].split(" ");
+            if(reportMap.get(sl[0]).contains(sl[1])){
+               continue; 
+            }
+            
+            reportMap.get(sl[0]).add(sl[1]);
+            alarmCnt.put(sl[1], alarmCnt.getOrDefault(sl[1], 0) +1);
         }
         
-        reportMap.forEach((reported, reporters) -> {
-          if(reporters.size() >= k){
-            reporters.forEach(reporter -> {
-              answer[nameIdx.get(reporter)]++;
-            });
-          }
-        });
-
+        Set<String> alarmId = new HashSet<>();
+        for(int i = 0 ; i < id_list.length ; i++){
+            int cnt = alarmCnt.getOrDefault(id_list[i], 0);
+            if(cnt >= k){
+                alarmId.add(id_list[i]);
+            }
+        }
+        for(int i = 0 ; i < id_list.length ; i++){
+            Set<String> myReport = reportMap.get(id_list[i]);
+            myReport.retainAll(alarmId);
+            answer[i] = myReport.size();
+        }
         return answer;
     }
 }
