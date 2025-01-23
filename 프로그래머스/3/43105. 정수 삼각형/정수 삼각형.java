42678158de80e9f45b17f6e89974ce1a4e67aa18
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2025-01-23 21:23:54 +0900

    [level 3] Title: 정수 삼각형, Time: 14.47 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,26 +1,18 @@
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
-        int h = triangle.length;
-        int[][] dp = new int[h][h];
-        
+        int n = triangle.length;
+        int[][] dp = new int[n+1][n];
         dp[0][0] = triangle[0][0];
-        for(int i = 1 ; i < h ; i++){
-            for(int j = 0 ; j <= i ; j++){
-                int m = 0;
-                if(j > 0 && j < i){
-                    m = Math.max(dp[i-1][j-1], dp[i-1][j]);
-                } else if (j == 0){
-                    m = dp[i-1][j];
-                } else if (j == i){
-                     m = dp[i-1][j-1];
-                }
-                dp[i][j] = m + triangle[i][j];
+        for(int i = 0 ; i < n-1 ; i++){
+            for(int j = 0; j <= i ; j++ ){
+                // 7 기준에서 밑에 왼쪽꺼랑 합친거랑, 밑에 왼쪽에 원래 있던거랑 비교해서 넣어
+                dp[i+1][j] = Math.max(dp[i+1][j], dp[i][j] + triangle[i+1][j]);
+                dp[i+1][j+1] = Math.max(dp[i+1][j+1], dp[i][j] + triangle[i+1][j+1]);
             }
         }
-        
-        for(int i = 0 ; i < h ; i ++){
-            answer = Math.max(answer, dp[h-1][i]);
+        for(int i = 0 ; i < n ; i++){
+            answer = Math.max(answer, dp[n-1][i]);
         }
         return answer;
     }
