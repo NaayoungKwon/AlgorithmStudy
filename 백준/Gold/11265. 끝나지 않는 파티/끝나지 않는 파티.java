45f98ce74f5f6c4e5bc8ee06dc021c0c11149032
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2025-01-26 14:50:50 +0900

    [Gold IV] Title: 끝나지 않는 파티, Time: 868 ms, Memory: 55720 KB -BaekjoonHub

--- 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
@@ -1,140 +1,128 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import javax.swing.text.DateFormatter;
 
 public class Main {
 
   private static BufferedReader br;
-  private static StringBuilder sb = new StringBuilder();
-  static StringBuilder S;
-  static StringBuilder T;
-
 
   public static void main(String[] args) throws IOException {
     br = new BufferedReader(new InputStreamReader(System.in));
 
-    String[] split = br.readLine().split(" ");
-    int n = Integer.parseInt(split[0]);
-    int m = Integer.parseInt(split[1]);
-    int[][] partyLine = new int[n+1][n+1];
-    for (int i = 1 ; i <= n ; i++){
-      String[] split1 = br.readLine().split(" ");
-      for(int j = 1 ; j <= n ; j++){
-        partyLine[i][j] = Integer.parseInt(split1[j-1]);
+    String[] line = br.readLine().split(" ");
+    int n = Integer.parseInt(line[0]);
+    int m = Integer.parseInt(line[1]);
+    int[][] pan = new int[n][n];
+//    int[] arr = new int[n];
+    Map<Integer, List<Path>> map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      String[] split = br.readLine().split(" ");
+      List<Path> sway = map.getOrDefault(i, new ArrayList<>());
+      for(int j = 0; j < n; j++){
+        if(i == j){
+          continue;
+        }
+        int d = Integer.parseInt(split[j]);
+        sway.add(new Path(j, d));
       }
+      map.put(i, sway);
+//      }
     }
 
-    List<Node> arr = new ArrayList<>();
-    for (int i = 0 ; i < m ; i++){
-      String[] split1 = br.readLine().split(" ");
-      arr.add(new Node(Integer.parseInt(split1[0]), Integer.parseInt(split1[1]), Integer.parseInt(split1[2])));
-    }
-    solution(n, m, partyLine, arr);
+    List<Way> customers = new ArrayList<>();
+    for(int i = 0 ; i < m ; i++){
+      String[] split = br.readLine().split(" ");
+      int s = Integer.parseInt(split[0]);
+      int e = Integer.parseInt(split[1]);
+      int d = Integer.parseInt(split[2]);
+      customers.add(new Way(s-1, e-1, d));
 
+    }
+//    System.out.println(solution(n, m , map, customers));
+    solution(n, m, map, customers);
   }
 
 
-  public static void solution(int n, int m, int[][] partyLine, List<Node> arr) {
-    Set<Integer> startSet = arr.stream().map(Node::getX).collect(Collectors.toSet());
-    Map<Integer, List<Integer>> djikstraMap = new HashMap<>();
+  public static void solution(int n, int m, Map<Integer, List<Path>> map, List<Way> customers){
+    int result = Integer.MAX_VALUE;
+    Map<Integer, int[]> dists = new HashMap<>();
 
-    startSet.forEach(start -> {
-      List<Integer> djikstra = djikstra(n, start, partyLine);
-      djikstraMap.put(start, djikstra);
-    });
+    for(int i : customers.stream().map(a -> a.s).collect(Collectors.toSet())){
+      int[] dist = dijkstra(n, i, map);
+      dists.put(i, dist);
+    }
 
-    arr.forEach(node -> {
-      if(djikstraMap.get(node.x).get(node.y) <= node.time){
+    for(Way customer : customers){
+      int[] dist = dists.get(customer.s);
+      dists.put(customer.s, dist);
+    }
+
+    for(Way c : customers){
+      if(dists.get(c.s)[c.e] <= c.d){
         System.out.println("Enjoy other party");
-      } else{
+      } else {
         System.out.println("Stay here");
       }
-    });
+    }
+
   }
 
-  public static List<Integer> djikstra(int n, int start,  int[][] partyLine){
-    PriorityQueue<Node> que = new PriorityQueue<>(Node::compareTo);
+  public static int[] dijkstra(int n, int start, Map<Integer, List<Path>> map){
     int[] dist = new int[n+1];
     Arrays.fill(dist, Integer.MAX_VALUE);
+    PriorityQueue<Path> pq = new PriorityQueue<>((a,b) -> a.d - b.d);
+
+    pq.add(new Path( start, 0));
     dist[start] = 0;
 
-    que.add(new Node(start, start, 0));
-
-    while(!que.isEmpty()){
-      Node node = que.poll();
-      if(dist[node.y] >= node.time){
-        for(int j = 1 ; j <= n ; j++){
-          if(j == start){
-            continue;
-          }
-          if(dist[j] > node.time + partyLine[node.y][j]){
-            dist[j] = node.time + partyLine[node.y][j];
-            que.add(new Node(node.y, j, dist[j]));
-          }
+    while(!pq.isEmpty()){
+      Path node = pq.poll();
+      if(dist[node.e] < node.d){
+        continue;
+      }
+      for(Path way : map.getOrDefault(node.e, new ArrayList<>())){
+        int cost = node.d + way.d;
+        if(cost < dist[way.e]){
+          dist[way.e] = cost;
+          pq.add(new Path(way.e, cost));
         }
-
       }
-
     }
-    return Arrays.stream(dist).boxed().collect(Collectors.toList());
+
+    return dist;
   }
 
 
-  public static class Node{
-    int x;
-    int y;
-    int time;
 
-    public Node(int x, int y, int time){
-      this.x = x;
-      this.y = y;
-      this.time = time;
+  public static class Path{
+    int e;
+    int d;
+
+    public Path(int e, int d){
+      this.e = e;
+      this.d = d;
     }
 
-    public int getX(){
-      return this.x;
-    }
-
-    public int getTime(){
-      return this.time;
-    }
-
-    @Override
-    public boolean equals(Object o){
-      if(o instanceof Node){
-        Node node = (Node) o;
-        return this.x == node.x && this.y == node.y;
-      }
-      return false;
-    }
-
-    public int compareTo(Node p) {
-
-      if(this.time < p.time){
-        return -1;
-      } else{
-        return 1;
-      }
-    }
   }
 
+  public static class Way{
+    int s;
+    int e;
+    int d;
+
+    public Way(int s, int e, int d){
+      this.s = s;
+      this.e = e;
+      this.d = d;
+    }
+
+  }
 
 }
