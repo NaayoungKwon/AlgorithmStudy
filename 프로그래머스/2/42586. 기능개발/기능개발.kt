8e4111f4b8c90ab2fd2573a244193229a93e4a69
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2025-08-05 00:15:20 +0900

    [level 2] Title: 기능개발, Time: 10.33 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.kt
+++ 프로그래머스/2/42586. 기능개발/기능개발.kt
@@ -1,27 +1,32 @@
 import java.util.*
-import kotlin.math.ceil
-
 class Solution {
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
     var answer = mutableListOf<Int>()
-
+    // 각각 몇일이 걸리는지를 계산한 배열을 만든다.
+    // 맨 첫 번째껄 일단 큐에 넣어놔
+    // 두 번째 요소부터 보는데, 큐에 있는 값 보다 작으면 넣어놔/ 만약 큐에 있는 값보다 크면 큐의 제일 앞의 값을 꺼내면서 나보다 큰 값이 나올 때까지 계속 빼
+    // 이렇게 뺀 것들의 수를 세서 answer에 넣는다.
     val daysToWork = mutableListOf<Int>()
     for(i in 0 until progresses.size) {
-        daysToWork.add(ceil(((100 - progresses[i]).toDouble() / speeds[i].toDouble())).toInt())
+        daysToWork.add(Math.ceil(((100 - progresses[i]).toDouble() / speeds[i].toDouble())).toInt())
     }
-    var queue: Queue<Int> = LinkedList()
+    val queue: Queue<Int> = LinkedList()
     for(i in 0 until progresses.size) {
         if(queue.isEmpty() || queue.peek() >= daysToWork[i]){
             queue.add(daysToWork[i])
         } else{
-            answer.add(queue.size)
-            queue = LinkedList()
+            var count = 0
+            while(queue.isNotEmpty() && queue.peek() < daysToWork[i]) {
+                queue.poll()
+                count += 1
+            }
+            answer.add(count)
             queue.add(daysToWork[i])
         }
     }
-    // if(answer.isNotEmpty()){
-    answer.add(queue.size)
-    // }
+    if(queue.isNotEmpty()){
+        answer.add(queue.size)
+    }
 //    println(answer.toString())
     return answer.toIntArray()
     }
