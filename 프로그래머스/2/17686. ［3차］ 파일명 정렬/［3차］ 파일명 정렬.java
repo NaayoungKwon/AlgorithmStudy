6170970610bbed106bf5bd8298c9182e99f49096
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2025-08-16 18:20:32 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 37.03 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,55 +1,39 @@
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 class Solution {
     public String[] solution(String[] files) {
-        String[] answer = {};
-        List<File> fileList = new ArrayList<>();
-
-        Pattern pattern = Pattern.compile("([a-zA-Z]+[a-zA-Z .-]*)([0-9]{1,5})(.*)");
-        for(int i = 0 ; i < files.length ; i++){
-            String file = files[i];
-            Matcher matcher = pattern.matcher(file);
-            matcher.find();
-            String head = matcher.group(1);
-            String number = matcher.group(2);
-            String tail = matcher.group(3);
-            fileList.add(new File(head, number, tail, i));
-        }
-        fileList.sort(File::compareTo);
-
-        return fileList.stream().map(File::toString).toArray(String[]::new);
+        return Arrays.stream(files).map(File::new).sorted(File::compareTo).map(File::getFullName).toArray(String[]::new);
     }
+    
+        public static class File {
 
-    public static class File implements Comparable<File>{
+        String fullName;
         String head;
-        String numberStr;
         Integer number;
         String tail;
-        Integer idx;
 
-        public File(String head, String numberStr, String tail, Integer idx){
-            this.head = head;
-            this.numberStr = numberStr;
-            this.number = Integer.valueOf(numberStr);
-            this.tail = tail;
-            this.idx = idx;
+        File(String fullName){
+            Pattern pattern = Pattern.compile("([a-zA-Z]+[a-zA-Z .-]*)([0-9]{1,5})(.*)");
+            Matcher matcher = pattern.matcher(fullName);
+            matcher.find();
+            this.head = matcher.group(1).toLowerCase();
+            this.number = Integer.parseInt(matcher.group(2));
+            this.tail = matcher.group(3);
+            this.fullName = fullName;
         }
 
-
-        @Override
-        public int compareTo(File o) {
-            int i = this.head.toLowerCase().compareTo(o.head.toLowerCase());
-            if(i != 0){
-                return i;
-            } else if (!Objects.equals(this.number, o.number)){
-                return this.number - o.number;
+        public Integer compareTo(File other){
+            if(this.head.equals(other.head)){
+                return this.number.compareTo(other.number);
+            } else {
+                return this.head.compareTo(other.head);
             }
-            return this.idx.compareTo(o.idx);
         }
 
-        public String toString(){
-            return head + numberStr + tail;
+        public String getFullName(){
+            return this.fullName;
         }
     }
 }
