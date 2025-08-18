6b6efde72fc8567082d0048841b129ae5c94dd2b
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2025-08-18 22:57:59 +0900

    [level 3] Title: 스티커 모으기(2), Time: 14.24 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,37 +1,26 @@
+import java.util.*;
 class Solution {
     public int solution(int sticker[]) {
         int answer = 0;
         int n = sticker.length;
-        // dp[이걸 땟을 때/안땟을 때][idx] = 최댓값
         int[][] dp = new int[2][n];
-
-        // 첫 번째 것을 무조건 뜯었을 때
-        dp[0][0] = 0;
-        dp[1][0] = sticker[0];
-        for(int i = 1 ; i < n-1 ; i++){
-            int left = i-1;
-            int right = (i + 1) % n;
-            dp[0][i] = Math.max(dp[0][left], dp[1][left]);
-            dp[1][i] = dp[0][left] + sticker[i];
-        }
-        if(n > 1){
-            answer = Math.max(answer, dp[0][n-2]);
-            answer = Math.max(answer, dp[1][n-2]);
-        } else {
-            answer = sticker[0];
-        }
-        
-
-        // 첫 번째 것을 안 뜯었을 때
         int[][] dp2 = new int[2][n];
-        for(int i = 1 ; i < n ; i++){
-            int left = i-1;
-            dp2[0][i] = Math.max(dp2[0][left], dp2[1][left]);
-            dp2[1][i] = dp2[0][left] + sticker[i];
-        }
-        answer = Math.max(answer, dp2[0][n-1]);
-        answer = Math.max(answer, dp2[1][n-1]);
 
-        return answer;
+        // dp : 안붙임, dp2 : 첫 번째 붙임
+        dp[0][0] = 0;
+        dp[1][0] = 0;
+        dp2[0][0] = 0;
+        dp2[1][0] = sticker[0];
+
+        for(int i = 1 ; i < n ; i++){
+            dp[0][i] = Math.max(dp[0][i-1], dp[1][i-1]);
+            dp[1][i] = dp[0][i-1] + sticker[i];
+
+            dp2[0][i] = Math.max(dp2[0][i-1], dp2[1][i-1]);
+            dp2[1][i] = dp2[0][i-1] + sticker[i];
+        }
+        dp2[1][n-1] = 0;
+
+        return n > 1 ? Math.max(dp2[0][n-1], Math.max(dp[1][n-1], dp[0][n-1])) : sticker[0];
     }
 }
