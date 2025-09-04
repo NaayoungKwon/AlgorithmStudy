845f7a3b183ae1f8644fea118fa0ca3966891e58
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2025-09-04 12:28:36 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.62 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,44 +1,40 @@
 from collections import Counter
 
 def solution(n, wires):
-    answer = 100000
-    # for문을 돌면서 해당 idx의 것이 없을 때 몇개로 나뉘는지 찾는다
-    wires.sort()
-    for exclude in range(n-1):
-        arr = [i for i in range(n+1)]
-        for i in range(n-1):
-            if i == exclude:
+    answer = -1
+    my_childs = [[] for _ in range(n+1)]
+    m = len(wires)
+    
+
+    mr = n * 100
+    for i in range(m):
+        parent = [0] * (n+1)
+        for a in range(1, n+1):
+            parent[a] = a
+        for j in range(m):
+            if i == j:
                 continue
-            arr = union(arr, wires[i][0], wires[i][1])
-        for i in range(1, n):
-            arr[i] = find(arr, i);
-            
+            union(parent, wires[j][0], wires[j][1])
+        
         a = 0
         b = 0
-        for i in range(1, n+1):
-            if find(arr, i) == 1:
+        for k in range(1, n+1):
+            if find(parent, k) != 1:
                 a += 1
-        b = n - a
-        answer = min(answer, abs(a-b))
-        # c = Counter(arr[1:]).most_common()
-        # if len(c) > 2:
-        #     continue
-            # print(exclude, arr)
-        # answer = min(answer, abs(c[0][1] - c[1][1]))
-            
-    return answer
+        b = n -a
+        mr = min(mr, abs(a-b))
 
-def union(arr, i, j):
-    a = find(arr, i)
-    b = find(arr, j)
-    if a < b:
-        arr[b] = a
-    else:
-        arr[a] = b
-    return  arr
+    return mr
 
-def find(arr, i):
-    if arr[i] != i:
-        arr[i] = find(arr, arr[i])
-    return arr[i]
-    
+def find(path, i):
+    if path[i] != i:
+        path[i] = find(path,path[i]);
+    return path[i];
+
+def union(path, a,b):
+    f_a = find(path, a);
+    f_b = find(path, b);
+    if f_a < f_b:
+        path[f_b] = f_a;
+    elif f_a > f_b:
+        path[f_a] = f_b;
