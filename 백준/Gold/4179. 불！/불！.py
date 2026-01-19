432cdc7aae736bc0975b9be77f92472b500df71d
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2026-01-20 00:37:07 +0900

    [Gold III] Title: 불!, Time: 460 ms, Memory: 175200 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,46 +1,58 @@
 import sys
 from collections import deque
 
-def solution(r,c,pan):
-    JIHOON, FIRES = (0,0), []
-    dir = [(1, 0), (-1,0), (0,1), (0,-1)]
-
-    for i in range(r):
-        for j in range(c):
-            if pan[i][j] == 'J':
-                pan[i][j] = 1
-                JIHOON = (i,j)
-            elif pan[i][j] == 'F':
-                pan[i][j] = '#'
-                FIRES.append((i,j))
-
+def solution(R, C, maze, hoon, fires):
+    dirs = [(0,1),(1,0),(0,-1),(-1,0)]
     que = deque()
-    que.append((JIHOON, 'J'))
-    for f in FIRES:
-        que.append((f, 'F'))
+    
+    for fire in fires:
+        que.append((fire, 'F', 1))
+    que.append((hoon, 'J', 1))
+
+    visited = [[False] * C for _ in range(R)]
+    visited[hoon[0]][hoon[1]] = True
 
     while que:
-        hoon, case= que.popleft()
-        if case == 'J' and (hoon[0] == 0 or hoon[0] == r-1 or hoon[1] == 0 or hoon[1] == c-1) and pan[hoon[0]][hoon[1]] != '#':
-            return pan[hoon[0]][hoon[1]] 
+        pos, type, l = que.popleft()
+        if type == 'J' and (pos[0] == 0 or pos[0] == R-1 or pos[1] == 0 or pos[1] == C-1):
+            return l
         
-        for d in dir:
-            nha, nhb = hoon[0] + d[0], hoon[1] + d[1]
-            if not (nha >= 0 and nha < r and nhb >= 0 and nhb < c):
+        for d in dirs:
+            nx, ny = pos[0] + d[0], pos[1] + d[1]
+            if not (0 <= nx < R and 0 <= ny < C):
                 continue
-    
+            if maze[nx][ny] == '#':
+                continue
             
-            if case == 'F' and pan[nha][nhb] != '#': #fire
-                pan[nha][nhb] = '#'
-                que.append(((nha, nhb), 'F'))
-            elif case == 'J' and pan[nha][nhb] == '.' and pan[hoon[0]][hoon[1]] != '#':
-                pan[nha][nhb] = pan[hoon[0]][hoon[1]] + 1
-                que.append(((nha, nhb), 'J'))
+            if type == 'F':
+                maze[nx][ny] = '#'
+                que.append(((nx, ny), type, l + 1))
+            elif type == 'J' and not visited[nx][ny]:
+                visited[nx][ny] = True
+                que.append(((nx, ny), type, l + 1))
+                if nx == 0 or nx == R-1 or ny == 0 or ny == C-1:
+                    return l + 1
+        # for a in maze:
+        #     print(''.join(a))
+        # print('---')
+    return "IMPOSSIBLE"
 
+input = sys.stdin.readline
 
-    return 'IMPOSSIBLE'
+R, C = map(int, input().split())
 
+maze = []
+hoon = None
+fires = set()
 
-r,c = map(int,sys.stdin.readline().split(' '));
-myList = [list(sys.stdin.readline())[:-1] for i in range(r)];
-print(solution(r, c, myList));
+for r in range(R):
+    row = input().rstrip()
+    maze.append(list(row))
+    for c, ch in enumerate(row):
+        if ch == 'J':
+            hoon = (r, c)
+        elif ch == 'F':
+            fires.add((r, c))
+            maze[r][c] = '#'
+
+print(solution(R, C, maze, hoon, fires))
