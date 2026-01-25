32c3c75e2fbb93808b50de663d4fd5ba042f85a9
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2026-01-25 15:04:24 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1476 ms, Memory: 322752 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,125 +1,99 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.time.Duration;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import javax.swing.text.DateFormatter;
+import java.util.stream.IntStream;
+
+import javax.management.RuntimeErrorException;
 
 public class Main {
 
-  private static BufferedReader br;
-  private static StringBuilder sb = new StringBuilder();
-  static StringBuilder S;
-  static StringBuilder T;
+    private static BufferedReader br;
+    private static StringBuilder sb = new StringBuilder();
+    static StringBuilder S;
+    static StringBuilder T;
 
-  public static void main(String[] args) throws IOException {
-    br = new BufferedReader(new InputStreamReader(System.in));
+    static List<Set<Integer>> combinationList = new ArrayList<>();
 
-    String[] split = br.readLine().split(" ");
-    int n = Integer.parseInt(split[0]);
-    int m = Integer.parseInt(split[1]);
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-    List<Path> paths = new ArrayList<>();
-    for (int i = 0 ; i < m ; i++){
-      String[] split1 = br.readLine().split(" ");
-      int a = Integer.parseInt(split1[0]);
-      int b = Integer.parseInt(split1[1]);
-      paths.add(new Path(Math.min(a,b), Math.max(a,b), Integer.parseInt(split1[2])));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+
+        List<P59> paths = new ArrayList<>();
+        for (int i = 0 ; i < m ; i++){
+            String[] split1 = br.readLine().split(" ");
+            int a = Integer.parseInt(split1[0]);
+            int b = Integer.parseInt(split1[1]);
+            paths.add(new P59(Math.min(a,b), Math.max(a,b), Integer.parseInt(split1[2])));
+        }
+        System.out.println(solution59(n,m, paths));
     }
 
-    System.out.println(solution(n, m, paths));
+    public static int solution59(int n, int m, List<P59> paths){
+        PriorityQueue<P59> pq = new PriorityQueue<>(P59::compareTo);
+        for(P59 p : paths){
+            pq.add(p);
+        }
 
-  }
+        int[] parent = new int[n+1];
+        for(int i = 0 ; i <= n ; i++){
+            parent[i] = i;
+        }
 
+        int totalWeight = 0;
+        int maxWeight = 0;
+        while(!pq.isEmpty()){
+            P59 now = pq.poll();
+            int fa = find(parent, now.start);
+            int fb = find(parent, now.end);
+            if(fa != fb){
+                union(parent, fa, fb);
+                totalWeight += now.weight;
+                maxWeight = Math.max(maxWeight, now.weight);
+            }
+        }
 
-
-  public static int solution(int n, int m, List< Path> paths) {
-    PriorityQueue<Path> pq = new PriorityQueue<>();
-    pq.addAll(paths);
-    List<Integer> count = new ArrayList<>();
-    int[] parent = new int[n+1];
-    for(int i = 0 ; i <= n ; i++){
-      parent[i] = i;
+        return totalWeight - maxWeight;
     }
 
+    public static class P59{
+        int start;
+        int end;
+        int weight;
 
-    while(!pq.isEmpty()){
-      Path path = pq.poll();
-      if(find(path.start, parent) != find(path.end, parent)){
-        unionFind(path.start, path.end, parent);
-        count.add(path.len);
-      }
+        public P59(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        public int compareTo(P59 other){
+            return Integer.compare(this.weight, other.weight);
+        }
+    }
+    
+        public static int find(int[] parent, int a){
+        if(parent[a] != a){
+            parent[a] = find(parent, parent[a]);
+        }
+        return parent[a];
     }
 
-    int sum = 0;
-    for(int i = 0 ; i < count.size() - 1 ; i++){
-      sum += count.get(i);
+    public static void union(int[] parent, int a, int b){
+        int pa = find(parent, a);
+        int pb = find(parent, b);
+        if(pa != pb){
+            parent[pb] = pa;
+        }
     }
-    return sum;
-  }
-
-  public static void  unionFind(int start , int end, int[] parent){
-
-    int sParent = find(start, parent);
-    int eParent = find(end, parent);
-    if( sParent != eParent ){
-      parent[Math.max(sParent, eParent)] = Math.min(sParent, eParent);
-    }
-
-  }
-
-  public static int find(int e , int[] parent){
-    if(parent[e] != e){
-      parent[e] = find(parent[e], parent);
-    }
-    return parent[e];
-  }
-
-  public static class Path implements Comparable<Path>{
-    int start;
-    int end;
-    int len;
-
-    public Path( int start, int end, int len){
-      this.len = len;
-      this.start = start;
-      this.end = end;
-    }
-
-    @Override
-    public int compareTo(Path p){
-      if(this.len < p.len){
-        return -1;
-      } else {
-        return 1;
-      }
-    }
-
-    @Override
-    public boolean equals(Object o){
-      if(o instanceof Path){
-        Path node = (Path) o;
-        return this.start == node.start && this.end == node.end && this.len == node.len;
-      }
-      return false;
-    }
-  }
-
-
 }
